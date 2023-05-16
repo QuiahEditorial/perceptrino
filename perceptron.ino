333f0a8e1acb40098ee557790f4ffc4f99c66fa8
Roberto A. Zavala <112892402+QuiahEditorial@users.noreply.github.com>
2023-05-15 22:00:49 -0600

    Update perceptron.ino
    El perceptrón termina su entrenamiento cuando los errores cometidos son cero.

--- perceptron.ino
+++ perceptron.ino
@@ -10,19 +10,19 @@ const int d = 3;                          // dimensión de las observaciones
 const int n = 4;                          // cantidad de observaciones
 
 float W[d] =      {0,0,0};                // pesos iniciales
-float A[n][d] = { {1,0,0},                // arreglo de entrenamiento
+float A[n][d] = { {1,0,0},                // Sea el arreglo de entrenamiento
                   {1,0,1},
                   {1,1,0},
                   {1,1,1} };
-float Z[n] =      {1,1,1,0};              // salidas esperadas
+float Z[n] =      {1,1,1,0};              // salidas etiquetadas manualmente
 float C[d] =      {0,0,0};                // clasificación
 float D;                                  // corrección
 float R;                                  // red
 float E;                                  // error
+float e;                                  // errores
 
-float S;                                  // suma
-int i,j;                                  // contadores
-int k = 8;                                // teorema del perceptrón
+float S;//[4] =      {0,0,0,0};           // sumas
+int i,j,k;                                // contadores
 
 void setup()
 {
@@ -31,16 +31,19 @@ Serial.begin(2000000);
 
 void loop()
 {
-for(k=0;k<=8;k++){for(j=0;j<=3;j++){for(i=0;i<=d-1;i++){
-  C[i]=W[i]*A[j][i];S+=C[i];} //Serial.print(S); Serial.print(" "); // S = \sum_{i=0}^{i=d} w_i x_{ji}
-  if(S>u){R=1;} else{R=0;}    //Serial.print(R); Serial.print(" "); // \phi(x) = cases 1,w^T x > u \\ 0, w^T x \leq u
-  S=0; E = Z[j]-R;            //Serial.print(E); Serial.print(" "); // e = Z-R
-  D = r*E;                    //Serial.print(D); Serial.print(" "); // W_i = W_i + x_i ( r e )
+iteracion:
+e=0;
+for(j=0;j<=3;j++){for(i=0;i<=d-1;i++){
+  C[i]=W[i]*A[j][i];S+=C[i];}  //Serial.print(S); Serial.print(" "); // S = \sum_{i=0}^{i=d} w_i x_{ji}
+  if(S>u){R=1;} else{R=0;} //Serial.print(R); Serial.print(" "); // \phi(x) = cases 1,w^T x > u \\ 0, w^T x \leq u
+  S=0; E = Z[j]-R;             //Serial.print(E); Serial.print(" "); // e = Z-R
+  if (E==0){ } else{e++;}
+  D = r*E;                     //Serial.print(D); Serial.print(" "); // W_i = W_i + x_i ( r e )
 for(i=0;i<=2;i++) {W[i]+=A[j][i]*D; Serial.print(W[i]);Serial.print(" ");}
                    Serial.write(10);
-                  }
+                 }
                    Serial.println("--------------");
-                  }
+if (e>0){goto iteracion;} else{Serial.println("Ok");}
 pctn();
 delay(10);
 exit(0);
